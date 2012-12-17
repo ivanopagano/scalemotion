90f92ef96df5f1723b3194d56c973aa3875f8c37
Ivano Pagano <pagoda_5b@hotmail.com>
2012-12-17 23:07:35 +0100

    simple parser for the stack overflow feed

--- build.sbt
+++ build.sbt
@@ -4,6 +4,9 @@ version := "0.0.1"
 
 scalaVersion := "2.9.2"
 
-libraryDependencies += "net.databinder.dispatch" %% "core" % "0.9.0"
-
-libraryDependencies += "org.scalatest" %% "scalatest" % "1.8" % "test"
+libraryDependencies ++= Seq(
+	"net.databinder.dispatch" %% "dispatch-core" % "0.9.5",
+	"org.scalatest" %% "scalatest" % "1.8" % "test",
+	"joda-time" % "joda-time" % "2.1",
+	"org.joda" % "joda-convert" % "1.2" % "compile"
+)
--- src/main/scala/it/pagoda5b/scalemotion/reader/RemoteSource.scala
+++ src/main/scala/it/pagoda5b/scalemotion/reader/RemoteSource.scala
@@ -1,5 +1,5 @@
 package it.pagoda5b.scalemotion.reader
-
+import org.joda.time._
 
 /**
  * La classe accede ad una sorgente di dati remota (un feed)
@@ -22,5 +22,59 @@ case class RemoteSource(urlString: String) {
  * Il parser estrae i dati utili dal contenuto della risposta remota.
  */
 trait ContentParser {
+
+	//Il tipo di entry del feed
+	type EntryType
+
+	//Il titolo
+	def parseTitle(root: xml.Elem): String
+
+	//Il numero di entries
+	def parseNumberOfEntries(root: xml.Elem): Int
+
+	//Genera la singola entry dall'elemento del feed
+	def parseEntry(entry: xml.Elem): EntryType
+
+}
+
+/**
+ * Raccoglie i dati di una entry nel feed
+ */
+case class FeedEntry(id: String, title: String, link: String, categories: Seq[String], author: String, published: DateTime, updated: DateTime)
+
+/**
+ * Parser specifico per il feed rss di StackOverflow
+ */
+trait SOFFeedParser extends ContentParser {
+
+	//Regular Expression per estrarre le date
+	private val DateExpression = """(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})Z""".r
+
+	//Converte la data in una DateTime di joda-time
+	private implicit def parseDate(date: String): DateTime = {
+			val DateExpression(year, month, day, hour, minutes, seconds) = date
+			new DateTime(year.toInt,
+					month.toInt,
+					day.toInt,
+					hour.toInt,
+					minutes.toInt,
+					seconds.toInt)
+	}
+
+	type EntryType = FeedEntry
+
+	def parseTitle(root: xml.Elem) = (root \\ "feed" \ "title").text
+
+	def parseNumberOfEntries(root: xml.Elem) = (root \\ "entry").size
+
+	def parseEntry(entry: xml.Elem) = FeedEntry(
+		id = (entry \ "id").text,
+		title = (entry \ "title").text,
+		link = (entry \ "link" \ "@href").text,
+		categories = (entry \ "category" \\ "@term") map (_.text),
+		author = (entry \ "author" \ "name").text,
+		published = (entry \ "published").text,
+		updated = (entry \ "updated").text
+		)
 
 }
