9252c91c02ff59bafbc8fa6869ad623824a8ed23
Ivano Pagano <pagoda_5b@hotmail.com>
2012-12-19 22:37:33 +0100

    autoformatted files with scalariform

--- src/main/scala/it/pagoda5b/scalemotion/reader/RemoteSource.scala
+++ src/main/scala/it/pagoda5b/scalemotion/reader/RemoteSource.scala
@@ -6,14 +6,14 @@ import org.joda.time._
  */
 case class RemoteSource(urlString: String) {
 
-	import dispatch._
-	
-	//il feed
-	val feed = url(urlString)
+  import dispatch._
 
-	/**
-	 * legge la sorgente in modo asincrono, restituendo una {{{Promise}}} del contenuto xml
-	 */
+  //il feed
+  val feed = url(urlString)
+
+  /**
+   * legge la sorgente in modo asincrono, restituendo una {{{Promise}}} del contenuto xml
+   */
   def read: Promise[xml.Elem] = Http(feed OK as.xml.Elem)
 
 }
@@ -23,14 +23,14 @@ case class RemoteSource(urlString: String) {
  */
 trait ContentParser[T] {
 
-	//Il titolo
-	def parseTitle(root: xml.Elem): String
+  //Il titolo
+  def parseTitle(root: xml.Elem): String
 
-	//Il numero di entries
-	def parseNumberOfEntries(root: xml.Elem): Int
+  //Il numero di entries
+  def parseNumberOfEntries(root: xml.Elem): Int
 
-	//Genera la singola entry dall'elemento del feed
-	def parseEntry(entry: xml.Elem): T
+  //Genera la singola entry dall'elemento del feed
+  def parseEntry(entry: xml.Elem): T
 
 }
 
@@ -44,32 +44,31 @@ case class FeedEntry(id: String, title: String, link: String, categories: Seq[St
  */
 trait SOFFeedParser extends ContentParser[FeedEntry] {
 
-	//Regular Expression per estrarre le date
-	private val DateExpression = """(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})Z""".r
+  //Regular Expression per estrarre le date
+  private val DateExpression = """(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})Z""".r
 
-	//Converte la data in una DateTime di joda-time
-	private implicit def parseDate(date: String): DateTime = {
-			val DateExpression(year, month, day, hour, minutes, seconds) = date
-			new DateTime(year.toInt,
-					month.toInt,
-					day.toInt,
-					hour.toInt,
-					minutes.toInt,
-					seconds.toInt)
-	}
+  //Converte la data in una DateTime di joda-time
+  private implicit def parseDate(date: String): DateTime = {
+    val DateExpression(year, month, day, hour, minutes, seconds) = date
+    new DateTime(year.toInt,
+      month.toInt,
+      day.toInt,
+      hour.toInt,
+      minutes.toInt,
+      seconds.toInt)
+  }
 
-	def parseTitle(root: xml.Elem) = (root \\ "feed" \ "title").text
+  def parseTitle(root: xml.Elem) = (root \\ "feed" \ "title").text
 
-	def parseNumberOfEntries(root: xml.Elem) = (root \\ "entry").size
+  def parseNumberOfEntries(root: xml.Elem) = (root \\ "entry").size
 
-	def parseEntry(entry: xml.Elem) = FeedEntry(
-		id = (entry \ "id").text,
-		title = (entry \ "title").text,
-		link = (entry \ "link" \ "@href").text,
-		categories = (entry \ "category" \\ "@term") map (_.text),
-		author = (entry \ "author" \ "name").text,
-		published = (entry \ "published").text,
-		updated = (entry \ "updated").text
-		)
+  def parseEntry(entry: xml.Elem) = FeedEntry(
+    id = (entry \ "id").text,
+    title = (entry \ "title").text,
+    link = (entry \ "link" \ "@href").text,
+    categories = (entry \ "category" \\ "@term") map (_.text),
+    author = (entry \ "author" \ "name").text,
+    published = (entry \ "published").text,
+    updated = (entry \ "updated").text)
 
 }
--- src/test/scala/it/pagoda5b/scalemotion/reader/ReaderTest.scala
+++ src/test/scala/it/pagoda5b/scalemotion/reader/ReaderTest.scala
@@ -1,62 +1,59 @@
 package it.pagoda5b.scalemotion.reader
 
-
 import org.scalatest._
 import org.scalatest.matchers._
 import xml._
 
 class RemoteSourceTest extends WordSpec with ShouldMatchers {
 
-	"A remote reader for scala site feed " should {
+  "A remote reader for scala site feed " should {
 
-		"read the expected xml feed" in {
-			val xmlTree: Option[Elem] = (new RemoteSource("http://www.scala-lang.org/rss.xml").read).option.apply()
+    "read the expected xml feed" in {
+      val xmlTree: Option[Elem] = (new RemoteSource("http://www.scala-lang.org/rss.xml").read).option.apply()
 
-			xmlTree should be ('defined)
-			xmlTree map (content => (content \\ "channel" \ "title").text) should be (Some("The Scala Programming Language"))
-			xmlTree map (content => (content \\ "channel" \ "link").text) should be (Some("http://www.scala-lang.org/"))
-		}
+      xmlTree should be('defined)
+      xmlTree map (content => (content \\ "channel" \ "title").text) should be(Some("The Scala Programming Language"))
+      xmlTree map (content => (content \\ "channel" \ "link").text) should be(Some("http://www.scala-lang.org/"))
+    }
 
-		"find the correct number of 24 entries" in {
-			val xmlTree: Option[Elem] = (new RemoteSource("http://www.scala-lang.org/rss.xml").read).option.apply()
+    "find the correct number of 24 entries" in {
+      val xmlTree: Option[Elem] = (new RemoteSource("http://www.scala-lang.org/rss.xml").read).option.apply()
 
-			xmlTree map (content => (content \\ "item").size) should be (Some(24))
+      xmlTree map (content => (content \\ "item").size) should be(Some(24))
 
-		}
+    }
 
-	}
+  }
 }
 
 class FeedParserTest extends WordSpec with ShouldMatchers {
 
-	"A feed parser" should {
+  "A feed parser" should {
 
-		import java.io.File
-		import org.joda.time._
+    import java.io.File
+    import org.joda.time._
 
-		val source: Elem = XML.loadFile(new File(getClass.getClassLoader.getResource("testFeed.xml").toURI))
-		val parser = new SOFFeedParser {}
+    val source: Elem = XML.loadFile(new File(getClass.getClassLoader.getResource("testFeed.xml").toURI))
+    val parser = new SOFFeedParser {}
 
-		"read the feed title" in {
-			(parser parseTitle source) should be ("Recent Questions - Stack Overflow")
-		}
-		"count the number of entries" in {
-			(parser parseNumberOfEntries source) should be (30)
-		}
-		"correctly read the first entry" in {
-			val entry = parser parseEntry (source \\ "entry").head.asInstanceOf[Elem]
-			entry should have (
-				'id ("http://stackoverflow.com/q/13919025"),
-				'title ("Visual c# Replace special characters and white space from a string"),
-				'link ("http://stackoverflow.com/questions/13919025/visual-c-sharp-replace-special-characters-and-white-space-from-a-string"),
-				'categories (Seq("c#","regex", "replace")),
-				'author ("user1346598"),
-				'published (new DateTime(2012,12,17,17,30,33)),
-				'updated (new DateTime(2012,12,17,17,30,33))
-			)
-		}
+    "read the feed title" in {
+      (parser parseTitle source) should be("Recent Questions - Stack Overflow")
+    }
+    "count the number of entries" in {
+      (parser parseNumberOfEntries source) should be(30)
+    }
+    "correctly read the first entry" in {
+      val entry = parser parseEntry (source \\ "entry").head.asInstanceOf[Elem]
+      entry should have(
+        'id("http://stackoverflow.com/q/13919025"),
+        'title("Visual c# Replace special characters and white space from a string"),
+        'link("http://stackoverflow.com/questions/13919025/visual-c-sharp-replace-special-characters-and-white-space-from-a-string"),
+        'categories(Seq("c#", "regex", "replace")),
+        'author("user1346598"),
+        'published(new DateTime(2012, 12, 17, 17, 30, 33)),
+        'updated(new DateTime(2012, 12, 17, 17, 30, 33)))
+    }
 
-	}
-
+  }
 
 }
